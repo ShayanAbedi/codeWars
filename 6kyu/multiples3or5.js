4c4d1cf060a0373c67110bbce0a458da36d8324a
ShayanAbedi <abedi.shayan@unb.ca>
2019-12-12 19:38:23 -0400

    6kyu - multiples 3 or 5

---
+++ 6kyu/multiples3or5.js
@@ -0,0 +1,11 @@
+function solution(number) {
+  let sum = 0;
+  for (let i = number - 1; i > 0; i--) {
+    if (i % 5 === 0 || i % 3 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(solution(10));
