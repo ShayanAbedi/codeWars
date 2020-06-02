0cbea1f8f36c226d055c0b4866a9ee3b3863933c
Shayan <abedi.shayan@unb.ca>
2020-06-01 21:04:45 -0300

    presistence - 6kyu - js

---
+++ 6kyu/presistentBugger.js
@@ -0,0 +1,19 @@
+function persistence(num) {
+  let count = 0;
+
+  while (num >= 10) {
+    let numbers = ("" + num).split("");
+    let multiple = 1;
+    numbers.forEach((n) => {
+      multiple = multiple * +n;
+    });
+    count++;
+    num = multiple;
+  }
+  return count;
+}
+
+console.log(persistence(39), 3);
+console.log(persistence(4), 0);
+console.log(persistence(25), 2);
+console.log(persistence(999), 4);
